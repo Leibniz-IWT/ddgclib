f16327dd7009e0aaa91e08f39dfc9deecc547a6f
ianto-cannon <ianto.cannon@gmail.com>
2025-02-13 18:24:19 +0100

    moving functions into library

--- ddgclib/_integrators.py
+++ ddgclib/_integrators.py
@@ -1,11 +1,13 @@
-def Euler(HC, tInit, nSteps, alpha, minEdge=-1, maxEdge=-1, targetVolume=-1)
+from ddgclib._bubble import save_vert_positions, get_forces, correct_the_volume
+
+def Euler(HC, tInit, nSteps, stepSize, minEdge=-1, maxEdge=-1, targetVolume=-1, constMoveLen=False):
   for t in range(tInit,tInit+nSteps+1):
     print('t',t,'nVerts',len(list(HC.V)),'maxMove',maxMove)
     if t%10==0: save_vert_positions(t)
     if minEdge>0: remesh(HC,minEdge,maxEdge)
     forceDict, maxForce = get_forces(HC, bV)
-    if constMove: alpha = maxMove/maxForce
+    if not constMoveLen: maxForce=1
     for v in HC.V:
       if v.x in forceDict:
-        HC.V.move(v, tuple(v.x_a + alpha*forceDict[v.x]))
+        HC.V.move(v, tuple(v.x_a + stepSize*forceDict[v.x]/maxForce))
     if targetVolume>0: correct_the_volume(HC, bV, targetVolume)
