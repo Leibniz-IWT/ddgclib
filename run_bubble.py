881b49ad5d1b35a2cfe223913fa525f7d28d8676
ianto-cannon <ianto.cannon@gmail.com>
2025-02-14 16:28:34 +0100

    Euler integration can be run

---
+++ run_bubble.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Imports and physical parameters
+#import copy
+#import sys
+import numpy as np
+import polyscope as ps
+
+# Local library
+#from ddgclib import *
+#from ddgclib._curvatures import HC_curvatures_sessile
+#from ddgclib._complex import *
+#from ddgclib._sphere import *
+#from ddgclib._sessile import *
+#from ddgclib._capillary_rise_flow import * #plot_surface#, curvature
+#from ddgclib._eos import *
+from ddgclib._plotting import plot_polyscope
+from ddgclib._bubble import triangle_prism_volume, cone_init, AdamsBashfortProfile
+from ddgclib._integrators import Euler
+
+
+# ### Parameters
+Bo=0 #Bond number
+RadTop = 1e-3 #(Bo*gamma/rho/g)**.5 # m Radius of curvature of bubble top
+params = {'P_0': 101.325e3,  # Pa, Ambient pressure
+'gamma': 72.8e-3,  # N/m, surface tension
+'g': 9.81}  # m/s2 gravitational acceleration
+params['rho']= Bo*params['gamma']/params['g']/RadTop**2 #998.2071 - 1.225 # kg/m3, density, STP
+print('rho',params['rho'])
+
+
+# define params tuple used in solver:
+params['initial_volume'], RadFoot = AdamsBashfortProfile(Bo, RadTop)
+print(f'RadFoot = {RadFoot}')
+minEdge = .1*RadFoot
+maxEdge = 2*minEdge
+maxMove = .5*minEdge**2/RadFoot
+t=0
+if t==0: HC,bV = cone_init(RadFoot, params['initial_volume'], NFoot=6, maxEdge=maxEdge)
+else: HC, bV = load_complex(t)
+#plot_polyscope(HC)
+print('vol area centroid', triangle_prism_volume(HC))
+Euler(HC, bV, params, t, 50, 0.1, implicitVolume=True, constMoveLen=True)
+#plot_polyscope(HC)
