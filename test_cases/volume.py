85a95c46580abaa75bd32466b9309c640be7f9ce
ianto-cannon <ianto.cannon@gmail.com>
2025-03-06 12:06:15 +0100

    halve the Adams Bashforth step size if the movement is too large

--- test_cases/volume.py
+++ test_cases/volume.py
@@ -7,7 +7,7 @@ import sys
 import os
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(SCRIPT_DIR))
-from ddgclib._volume import spherical_cap_init, cone_init, sector_volume, prism_volume, triangle_prism_volume
+from ddgclib._volume import spherical_cap_init, spherical_cap_contact_angle, cone_init, sector_volume, prism_volume, triangle_prism_volume
 
 print('make conical complex')
 HC,bV = cone_init(1, 1, maxEdge=.2)
@@ -17,7 +17,8 @@ print('triangle_prism_volume error ', triangle_prism_volume(HC)[0]-1)
 
 print('')
 print('make spherical cap complex')
-HC,bV = spherical_cap_init(1, 1, maxEdge=.2)
+RadSphere, contactAng = spherical_cap_contact_angle(.1 ,1)
+HC,bV = spherical_cap_init(RadSphere, contactAng, maxEdge=.2)
 print('sector_volume error ', sector_volume(HC)-1)
 print('prism_volume error ', prism_volume(HC)-1)
 print('triangle_prism_volume error ', triangle_prism_volume(HC)[0]-1)
