08742c09b13c83f7632fa8989715d9c7f6365a5b
Stefan Endres <stefan.c.endres@gmail.com>
2026-01-20 11:37:46 +0100

    Add updated manuscript figures

---
+++ manuscript_figures/fig11_de.py
@@ -0,0 +1,153 @@
+import numpy as np
+import scipy
+
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.patches import FancyArrowPatch
+from mpl_toolkits.mplot3d import proj3d
+from ipywidgets import *
+from matplotlib import pyplot as plt, ticker as mticker
+
+# ddg imports
+# Allow for relative imports from main library:
+import sys
+import os
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.dirname(SCRIPT_DIR))
+from ddgclib import *
+from ddgclib._complex import *
+from ddgclib._curvatures import *  # plot_surface#, curvature
+from ddgclib._capillary_rise_flow import * #plot_surface#, curvature
+from ddgclib._eos import *
+from ddgclib._misc import *
+from ddgclib._plotting import *
+
+matplotlib.rcParams['font.size'] = 16
+
+
+# Parameters for a water droplet in air at standard laboratory conditions
+gamma = 0.0728  # N/m, surface tension of water at 20 deg C
+rho = 1000  # kg/m3, density
+g = 9.81  # m/s2
+
+# Colour scheme for surfaces
+db = np.array([129, 160, 189]) / 255  # Dark blue
+lb = np.array([176, 206, 234]) / 255  # Light blue
+
+cdist = 1e-12
+
+# Replot error data
+Nmax = 21
+#lp_error = np.zeros(Nmax - 3)
+Nlist = list((range(3, Nmax)))
+Nlist = list((range(4, Nmax)))
+
+erange = []
+lp_error = []
+Nmax = 21
+refinement = 1
+phi = 2
+r = 0.5e-3  # m, Radius of the capillary tube
+#theta_p = 20.001 * np.pi / 180.0  # Three phase contact angle
+theta_p = 45 * np.pi / 180.0  # Three phase contact angle
+R = r / np.cos(theta_p)  # = R at theta = 0
+
+# Note the capillary rise systems can be visualized with this code at any N/refinement:
+if 0:
+    N = 8
+    fig, axes, HC = cape_rise_plot(r, theta_p, gamma, N=N, refinement=refinement)
+    axes.grid(False)
+    axes.set_xticks([])
+    axes.set_yticks([])
+    axes.set_zticks([])
+    plt.axis('off')
+
+#for N in range(4, Nmax + 1):
+for N in range(5, Nmax + 1):
+    F, nn, HC, bV, K_f, H_f = cap_rise_init_N(r, theta_p, gamma, N=N,
+                                              refinement=refinement,
+                                              cdist=cdist
+                                              )
+
+    (HNda_v_cache, K_H_cache, C_ijk_v_cache, HN_i,  HNdA_ij_dot_hnda_i,
+     K_H_2, HNdA_i_Cij) = int_curvatures(HC, bV, r, theta_p, printout=0)
+    # Laplacian error
+    P_L = gamma*(2.0/R)  # Analytical solution
+    H_dis = HNdA_ij_dot_hnda_i[0]
+    L_error = 100 *(P_L - gamma * H_dis)/(P_L)
+    lp_error.append(abs(L_error))
+    max_int_e = 0.0
+    ern = 0.0
+    for v in bV:
+        for v2 in v.nn:
+            if v2 in bV:
+                a = v.x_a
+                b = v2.x_a
+               # ern = 0.5 * numpy.linalg.norm(a - b) ** 2 / (12 * (2)**2)
+                ern = 2 * numpy.linalg.norm(a - b) ** 2 / (12 * (2)**2)
+                max_int_e += ern
+
+    erange.append( (max_int_e/r)/len(bV) )
+
+# Computed from step size on Euclidean metric for cap rise:
+geo_error = erange
+
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+#ax2 = ax.twinx()
+print(f'lp_error = {lp_error}')
+ln1 = ax.loglog(Nlist, np.abs(lp_error), 'x', color='tab:blue',
+             label='Young-Laplace Fehler: $(F_{cap} - \hat{F_{cap}}) / F_{cap} $')
+
+#ax2.set_ylabel(r'Young-Laplace Fehler (%)')
+ax.set_xlabel(r'$n$ (Anzahl der Scheitelpunkte)')
+
+eps = (geo_error[-1] - geo_error[0])/(Nlist[-1] - Nlist[0])
+print(f' eps = {eps**(-1)}')
+#ln2 = ax.loglog(Nlist, geo_error, 'X', color='tab:orange',
+#          label=f'Integration error (p-normed trapezoidal rule $O(h^2)$)')
+ax.set_ylabel(r'Young-Laplace Fehler (%)')
+
+x, y = Nlist, geo_error
+x, y = np.float32(x), np.float32(y)
+z = numpy.polyfit(x, y, 1)
+z = np.polyfit(np.log10(x), np.log10(y), 1)
+
+p = numpy.poly1d(z)
+p = np.poly1d(z)
+# the line equation:
+print("y=%.6fx+(%.6f)" % (z[0], z[1]))
+#ax2.set_ylim([1e-15, 1e-13])
+lns = ln1 #+ ln2
+labs = [l.get_label() for l in lns]
+ax.legend(lns, labs, loc=0, prop={'size': 15})
+
+plt.tick_params(axis='y', which='minor')
+
+
+ax.xaxis.set_minor_formatter(mticker.ScalarFormatter())
+#ax2.xaxis.set_minor_formatter(mticker.ScalarFormatter())
+
+plt.tick_params(axis='y', which='minor')
+
+plt.tight_layout()
+
+plt.savefig('fig10.png', bbox_inches='tight', dpi=600)
+
+
+
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ manuscript_figures/fig19_droplet_data.py
@@ -0,0 +1,136 @@
+"""
+Plots using the data output from running droplet.py for the given refinements
+ and parameters sets. To obtain this data droplet.py must be run switching the
+ different parameter sets on/off for the 3 refinements listed in the file.
+
+"""
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.patches import FancyArrowPatch
+from mpl_toolkits.mplot3d import proj3d
+from ipywidgets import *
+from matplotlib.widgets import Slider
+from matplotlib import pyplot as plt, ticker as mticker
+
+# ddg imports
+# Allow for relative imports from main library:
+import sys
+import os
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.dirname(SCRIPT_DIR))
+from ddgclib import *
+from ddgclib._complex import *
+from ddgclib._curvatures import * #plot_surface#, curvature
+from ddgclib._capillary_rise_flow import * #plot_surface#, curvature
+from ddgclib._eos import *
+from ddgclib._misc import *
+from ddgclib._plotting import *
+# Our formulation (droplet.py)
+N_list = [7, 19, 61]  # Total number of vertices at end
+bV_list = [6, 12, 24]  #
+# Average data norm error:
+
+data_error = [2.5271702590869453e-06,
+              2.5271699752705548e-06,
+              9.175761973317646e-06]
+
+# Average relative angle error
+phi_rel_error = [2.8381639047890193e-13,
+                 0.0003696689325014279,
+                 0.02953080254880999]
+
+# Surface evolver
+se_N_list = [12, 39, 141]
+se_data_error = [0.010929308788062657, 0.003524668110821649,
+                 0.0022713988668246385]
+
+# Avg data error = 0.0020213679022338337
+# HC.V.size() = 9873
+
+
+if 0:
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    ax2 = ax.twinx()
+    ln2 = ax2.loglog(N_list, data_error, 'x', color='tab:blue',
+                     #label=r'Relative error in DDG formulation $\min _{i, j}  \frac{|| \bf{x_i} - \bf{x_j^*} ||}{\bf{x_j^*}}$')
+                     label=r'Relative error in DDG formulation')
+
+    ax2.set_ylabel(r'Relative error (DDG formulation)')
+    ax.set_xlabel(r'$n$ (number of vertices)')
+    ln1 = ax.loglog(se_N_list, se_data_error, 'X', color='tab:orange',
+                    #label=r'Surface Evolver error $\min _{i, j}  \frac{|| \bf{x_i} - \bf{x_j^*} ||}{\bf{x_j^*}}$')
+                    label=r'Relative error in Surface Evolver')
+    ax.set_ylabel(f'Relative error (Surface Evolver)')
+
+
+    ax2.set_ylim([1e-6, 5e-5])
+    ax.set_ylim([1e-3, 1e-1])
+    lns = ln1 + ln2
+    labs = [l.get_label() for l in lns]
+    ax.legend(lns, labs)
+
+    plt.tick_params(axis='y', which='minor')
+
+    ax.xaxis.set_minor_formatter(mticker.ScalarFormatter())
+    ax2.xaxis.set_minor_formatter(mticker.ScalarFormatter())
+
+    plt.tick_params(axis='y', which='minor')
+else:
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    #ax = ax.twinx()
+    ln2 = ax.loglog(N_list, data_error, 'x', color='tab:blue',
+                     #label=r'Relative error in DDG formulation $\min _{i, j}  \frac{|| \bf{x_i} - \bf{x_j^*} ||}{\bf{x_j^*}}$')
+                     label=r'Relative error in DDG formulation')
+
+    ax.set_ylabel(r'Relative error (DDG formulation)')
+    ax.set_xlabel(r'$n$ (number of vertices)')
+    ln1 = ax.loglog(se_N_list, se_data_error, 'X', color='tab:orange',
+                    #label=r'Surface Evolver error $\min _{i, j}  \frac{|| \bf{x_i} - \bf{x_j^*} ||}{\bf{x_j^*}}$')
+                    label=r'Relative error in Surface Evolver')
+    ax.set_ylabel(f'Relative error')
+
+
+    #ax2.set_ylim([1e-6, 5e-5])
+    #ax.set_ylim([1e-3, 1e-1])
+    ax.set_ylim([1e-6, 1e-1])
+    lns = ln1 + ln2
+    labs = [l.get_label() for l in lns]
+    ax.legend(lns, labs)
+
+    plt.tick_params(axis='y', which='minor')
+
+    ax.xaxis.set_minor_formatter(mticker.ScalarFormatter())
+    ax.xaxis.set_minor_formatter(mticker.ScalarFormatter())
+
+    plt.tick_params(axis='x', which='minor')
+
+if 0:
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    #ax2 = ax.twinx()
+    ln1 = ax.loglog(N_list, data_error, 'x', color='tab:blue',
+                     label='Error 1 $')
+
+    ax.set_ylabel(r'Young-Laplace error (%)')
+    ax.set_xlabel(r'$n$ (number of vertices)')
+    ln2 = ax.loglog(se_N_list, se_data_error, 'X', color='tab:orange',
+                    label=f'Error 2 surface evovler)')
+    ax.set_ylabel(f'Integration error (%)')
+
+    #ax.set_ylim([1e-1, 1e-7])
+    lns = ln2 + ln1
+    labs = [l.get_label() for l in lns]
+    ax.legend(lns, labs)
+
+    plt.tick_params(axis='y', which='minor')
+
+    ax.xaxis.set_minor_formatter(mticker.ScalarFormatter())
+    #ax2.xaxis.set_minor_formatter(mticker.ScalarFormatter())
+
+    plt.tick_params(axis='y', which='minor')
+
+    #plt.show()
+
+plt.show()
