026ac3a6d4c7c8cb38693e1a1ff8ad3dbed2ab40
Stefan Endres <stefan.c.endres@gmail.com>
2026-01-20 10:21:30 +0100

    Add mean flow folder

---
+++ ddgclib/operators/__init__.py
@@ -0,0 +1,12 @@
+#from ddgclib._curvatures import *
+#from ddgclib._capillary_rise import *
+#from ddgclib._cube_droplet import *
+
+__all__ = [s for s in dir() if not s.startswith('_')]
+
+#from ._eos import *
+#from ._complex import *
+#from ._curvatures import * #plot_surface#, curvature
+#from ._capillary_rise_flow import * #plot_surface#, curvature
+#from ._eos import *
+#from ._misc import *
