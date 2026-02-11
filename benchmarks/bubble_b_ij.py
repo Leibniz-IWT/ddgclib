7b61f58c520ac48d385c9856c8ec5da7d43c6b23
Stefan Endres <stefan.c.endres@gmail.com>
2026-02-11 10:51:02 +0100

    ENH: Add bubble to benchmark files

--- benchmarks/bubble_b_ij.py
+++ benchmarks/bubble_b_ij.py
@@ -9,11 +9,10 @@ import polyscope as ps
 # Local library imports
 from ddgclib import *
 from ddgclib._curvatures import HC_curvatures_sessile
-from ddgclib._complex import *
+from hyperct import *
 from ddgclib._sphere import *
 from ddgclib._sessile import *
 from ddgclib._capillary_rise_flow import *  # plot_surface, curvature
-from ddgclib._eos import *
 from ddgclib._plotting import plot_polyscope
 
 
--- benchmarks/bubble_heron.py
+++ benchmarks/bubble_heron.py
@@ -10,11 +10,10 @@ import polyscope as ps
 from ddgclib import *
 from ddgclib._curvatures import HC_curvatures_sessile
 from ddgclib._curvatures_heron import hndA_i
-from ddgclib._complex import *
+from hyperct import *
 from ddgclib._sphere import *
 from ddgclib._sessile import *
 from ddgclib._capillary_rise_flow import *  # plot_surface, curvature
-from ddgclib._eos import *
 from ddgclib._plotting import plot_polyscope
 
 
